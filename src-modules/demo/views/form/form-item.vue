c0aa989c5674ae34efdb5e39e25294fdde116e2d
liuk123 <40844000+liuk123@users.noreply.github.com>
2023-03-06 17:43:30 +0800

    1

---
+++ src-modules/demo/views/form/form-item.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+<script>
+export default {
+  name: '',
+  components: {},
+  data () {
+    return {
+      formData: {
+        
+      }
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
